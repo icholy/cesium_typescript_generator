097b8ad3d92e55d902b28715c7f5c3cc587bd27e
Ilia Choly <ilia.choly@gmail.com>
2015-05-20 13:19:01 -0400

    track typings with git

---
+++ typings/jasmine-matchers.d.ts
@@ -0,0 +1,8 @@
+declare function defineSuite(...args: any[]): any;
+declare function expect(...args: any[]): any;
+declare function it(...args: any[]): any;
+declare function afterEach(...args: any[]): any;
+declare function beforeEach(...args: any[]): any;
+declare function spyOn(...args: any[]): any;
+declare function describe(...args: any[]): any;
+
---
+++ typings/tsd.d.ts
@@ -0,0 +1,4 @@
+
+/// <reference path="es6-promise/es6-promise.d.ts" />
+/// <reference path="requirejs/require.d.ts" />
+/// <reference path="jasmine-matchers.d.ts" />
